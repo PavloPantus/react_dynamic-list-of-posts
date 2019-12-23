4cb2ae6b7e40c2b13da70b9f79f8ba38df76621e
PavloPantus <mr.pantus@gmail.com>
2019-12-24 00:30:06 +0200

    Updates

---
+++ precache-manifest.97671cd65621debdbe76b051637218f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9138b1f49551b21054251aa3006d1498",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "f464326b5b32cf5172af",
+    "url": "/react_dynamic-list-of-posts/static/css/main.c4d56fdd.chunk.css"
+  },
+  {
+    "revision": "56ca03f88e34a54573be",
+    "url": "/react_dynamic-list-of-posts/static/js/2.15c2d62f.chunk.js"
+  },
+  {
+    "revision": "f464326b5b32cf5172af",
+    "url": "/react_dynamic-list-of-posts/static/js/main.bd4c6849.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
